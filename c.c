fbccc14429199fcafdf7faef47cbe7b1206ce23c
sadraadibi <sadraadibi@aut.ac.ir>
2020-01-04 12:06:49 +0330

    Create c.c

---
+++ c.c
@@ -0,0 +1,70 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>
+char map[4][4]={{'0','1','*','1'},{'1','#','1','1'},{'1','1','1','1'},{'#','1','*','1'}};
+void get_file (void){
+//	  FILE *file;
+  //file=fopen("//home//kalagh//testcases//testcase01.txt", "r");
+ // static 
+}
+////////////////////////////////////////////////////////////////////////////////////////
+int random (){int q;
+   time_t t;
+   srand((unsigned) time(&t));
+      return ((rand() % 4)+1);
+}
+
+void SADRA(){ int i,j;char c;
+////////////////////////////////
+	while (1){
+		    for( i=0 ;i<3; i++) {
+        for( j=0; j<3; j++) {
+				c=getchar();
+				if (c== '0'){break;}
+        } }	}
+        /////////////////////find packman
+        
+
+
+
+}// sadra
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+int main () {SADRA();}//sadra caller
