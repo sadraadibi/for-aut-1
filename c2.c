e53054105ab1d8697af1ac9aba9b781e3868cfcc
sadraadibi <59312119+sadraadibi@users.noreply.github.com>
2020-01-14 12:41:07 +0330

    Update c2.c

--- c2.c
+++ c2.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-#include <windows.h>
 
 #define xxxxxxxx 10
 #define yyyyyyyy 10
 
 
 
-//2 -> pacman
-//1 -> fruit
-//0 -> block
-//5 -> empty
+//2 === pacman
+//1 === food
+//0 === block
+//5 === empty
 
 
 
 
 
-int pacmanx=0;
-int pacmany=0;
+int pacmanx=1;
+int pacmany=1;
 int count=0;
 
 ////////////////////////////////////////////
-void move (int pacmanx,int pacmany , int map [yyyyyyyy][xxxxxxxx]) {
+void move (int map [yyyyyyyy][xxxxxxxx]) {
+		getch();
 		int key = getch();
+		system ("cls");
 		map[pacmany][pacmanx] = 5;
 	switch(key) {
 		case 72: 			if (pacmany > 0 && map[pacmany-1][pacmanx])pacmany--;
@@ -33,7 +34,8 @@ void move (int pacmanx,int pacmany , int map [yyyyyyyy][xxxxxxxx]) {
 				map[pacmany][pacmanx] = 2;
 			break;//DOWN 
 		case 75: 		    if (pacmanx > 0 && map[pacmany][pacmanx-1])pacmanx--;
-				map[pacmany][pacmanx] = 2;;//LEFT
+				map[pacmany][pacmanx] = 2;
+			break;//LEFT
 		case 77: 			if (pacmanx + 1 < xxxxxxxx && map[pacmany][pacmanx+1])pacmanx++;
 				map[pacmany][pacmanx] = 2;	
 			break;//RIGHT
@@ -86,8 +88,8 @@ int main () {
 								};
 	printmap (map);
 	while(1){
-		move(pacmanx , pacmany , map [yyyyyyyy][xxxxxxxx]);
-		printmap( map[yyyyyyyy][xxxxxxxx]);
+		move(map);
+		printmap( map);
 	}
 
 }
