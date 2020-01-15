aea2302013c5d4e054baeb8d07f998fb793120b9
sadraadibi <59312119+sadraadibi@users.noreply.github.com>
2020-01-15 15:57:00 +0330

    full
    2part

--- c2.c
+++ c2.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
 
 #define xxxxxxxx 10
 #define yyyyyyyy 10
 
-
+HANDLE hConsole;
 
 //2 === pacman
 //1 === food
@@ -50,18 +53,19 @@ void move (int map [yyyyyyyy][xxxxxxxx]) {
 ///////////////////////////////////////////////
 void printmap (int map[yyyyyyyy][xxxxxxxx]) {
 	for (int i = 0; i < yyyyyyyy; i++){
-		for (int j = 0; j < xxxxxxxx; j++) {
+		for (int j = 0; j < xxxxxxxx; j++) {SetConsoleTextAttribute(hConsole, 7);
 			char box = ' ';
 			if (!map [i][j]) {box = 219;
 
-			}else if(map [i][j] == 2 && count%2==0) {
+			}else if(map [i][j] == 2 && count%2==0) {SetConsoleTextAttribute(hConsole, 14);
 				box = 99;count++;
-			}else if(map [i][j] == 2 && count%2==1) {
+			}else if(map [i][j] == 2 && count%2==1) {SetConsoleTextAttribute(hConsole, 14);
 				box = 67;count++;
-			}else if (map [i][j] == 1)
-				box = 248; else box = 32;
+			}else if (map [i][j] == 1){
+				box = 248;SetConsoleTextAttribute(hConsole, 12);} else box = 32;
 			
 			printf ("%c", box);
+			
 		}
 		printf ("\n");
 	}
@@ -69,9 +73,21 @@ void printmap (int map[yyyyyyyy][xxxxxxxx]) {
 
 
 //////////////////////////////////////
+int food (int map[yyyyyyyy][xxxxxxxx]) {
+		for (int i = 0; i < yyyyyyyy; i++){
+		for (int j = 0; j < xxxxxxxx; j++) {
+			if (map [i][j]== 1) return 1;
+		}}
+		return 0;}
+			
+			
+			
+			
+			
+//////////////////////////////////////
 
-int main () {
-	
+int main () {int x=1;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 	
 	
 	int map [yyyyyyyy][xxxxxxxx] = {
@@ -87,9 +103,59 @@ int main () {
 							    	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 								};
 	printmap (map);
-	while(1){
+	while(x){
 		move(map);
 		printmap( map);
+		x=food(map);
 	}
-
+printf("                                                                                                              \n ******  ****** ********** ************** ************** ************** ****************   ********  ******** \n ******  ****** ********** ************** ************** ************** ****************   ********  ******** \n ******  ****** ********** ************** ************** ************** ****************   ********  ******** \n ******  ******   ******   ******             ******     ******  ****** ******    ******     **************   \n ******  ******   ******   ******             ******     ******  ****** ****************     **************   \n ******  ******   ******   ******             ******     ******  ****** ****************       **********     \n ******  ******   ******   ******             ******     ******  ****** ****************         ******       \n **************   ******   ******             ******     ******  ****** ******  ******           ******       \n ************** ********** **************     ******     ************** ******  **********       ******       \n   **********   ********** **************     ******     ************** ******  **********       ******       \n     ******     ********** **************     ******     ************** ******  **********       ******       \n                                                                                                              \n");
+getch();
 }
+
+
+
+
+
+/*
+int main () {
+
+	char fileAddress [MAX];
+	
+	printf ("ENTER SOURCE FILE ADDRESS : ");
+	gets (fileAddress);
+
+	system ("cls");
+}
+void scanTable (coord* pacman, short table[MCOLUMN][MROW], char const* fileAddress){
+	FILE* source = fopen (fileAddress, "r");
+
+	int i = 0;
+	int j = 0;
+
+	if (source == NULL) {
+		SetConsoleTextAttribute(hConsole, 76);
+		printf ("FIle Not Exist\n");
+		SetConsoleTextAttribute(hConsole, 15);
+		exit (1);
+	}
+	while (!feof (source)){
+		char bfr = getc (source);
+
+		if (bfr == '*') table [i][j++] = 1;
+		if (bfr == '#') table [i][j++] = 0;
+		if (bfr == '1') table [i][j++] = -1;
+		if (bfr == '0') {
+			table [i][j++] = 2;
+			pacman->x = j-1;
+			pacman->y = i;
+		}
+
+		else if (bfr == '\n') {
+			i++;
+			if (j > ROW) ROW = j; j = 0;
+		}
+	}
+}
+
+
+*/
