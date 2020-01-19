5090baf24f5f305955c43e9bb7671a1375d3cbe0
sadraadibi <59312119+sadraadibi@users.noreply.github.com>
2020-01-19 11:01:34 +0330

    done

--- project 2.c
+++ project 2.c
@@ -12,17 +12,47 @@ HANDLE hConsole;
 
 //2 === pacman
 //1 === food
-//0 === block
+//8 === block
 //5 === empty
-
+//0 === out of reng
 
 
 
 
 int pacmanx=1;
 int pacmany=1;
-int count=0;
+int count=1;
+////////////////////////////////////////////
+void scanmap (int map[yyyyyyyy][xxxxxxxx], char const* fileAddress){
+	FILE* source = fopen (fileAddress, "r");
+	int count=0;
+	int i = 0;
+	int j = 0;
 
+	if (source == NULL) {
+		printf ("FIle Dont Exist\n");
+		exit (1);
+	}
+	while (!feof (source)){
+		char bfr = getc (source);
+
+		if (bfr == '*') map [i][j++] = 1;
+		if (bfr == '#') map [i][j++] = 8;
+		if (bfr == '1') map [i][j++] = 5;
+		if (bfr == '0'){
+		map [i][j++] = 2;
+		pacmanx=j;
+		pacmany=i;}
+		if (bfr == '\n' ) {
+		i++;j=0 ;}
+	}
+	
+	
+	
+	
+	
+	
+	}
 ////////////////////////////////////////////
 void move (int map [yyyyyyyy][xxxxxxxx]) {
 		getch();
@@ -30,20 +60,20 @@ void move (int map [yyyyyyyy][xxxxxxxx]) {
 		system ("cls");
 		map[pacmany][pacmanx] = 5;
 	switch(key) {
-		case 72: 			if (pacmany > 0 && map[pacmany-1][pacmanx])pacmany--;
+		case 72: 			if (pacmany > 0 && map[pacmany-1][pacmanx] && map[pacmany-1][pacmanx]!=8)pacmany--;
 				map[pacmany][pacmanx] = 2;
 			break;;//UP 
-		case 80: 			if (pacmany + 1 < yyyyyyyy && map[pacmany+1][pacmanx])pacmany++;
+		case 80: 			if ( map[pacmany+1][pacmanx] && map[pacmany+1][pacmanx]!=8)pacmany++;
 				map[pacmany][pacmanx] = 2;
 			break;//DOWN 
-		case 75: 		    if (pacmanx > 0 && map[pacmany][pacmanx-1])pacmanx--;
+		case 75: 		    if (pacmanx > 0 && map[pacmany][pacmanx-1] && map[pacmany][pacmanx-1]!=8)pacmanx--;
 				map[pacmany][pacmanx] = 2;
 			break;//LEFT
-		case 77: 			if (pacmanx + 1 < xxxxxxxx && map[pacmany][pacmanx+1])pacmanx++;
+		case 77: 			if ( map[pacmany][pacmanx+1] && map[pacmany][pacmanx+1]!=8)pacmanx++;
 				map[pacmany][pacmanx] = 2;	
 			break;//RIGHT
-		
-		default:printf("h") ;
+				case 45:printf("see you soon") ; exit (1);	break;
+		default:printf("keyboard arrow keys\n") ;
 	}
 	
 
@@ -55,14 +85,15 @@ void printmap (int map[yyyyyyyy][xxxxxxxx]) {
 	for (int i = 0; i < yyyyyyyy; i++){
 		for (int j = 0; j < xxxxxxxx; j++) {SetConsoleTextAttribute(hConsole, 7);
 			char box = ' ';
-			if (!map [i][j]) {box = 219;
+			if (map [i][j]==8) {box = 219;
 
 			}else if(map [i][j] == 2 && count%2==0) {SetConsoleTextAttribute(hConsole, 14);
 				box = 99;count++;
 			}else if(map [i][j] == 2 && count%2==1) {SetConsoleTextAttribute(hConsole, 14);
 				box = 67;count++;
 			}else if (map [i][j] == 1){
-				box = 248;SetConsoleTextAttribute(hConsole, 12);} else box = 32;
+				box = 248;SetConsoleTextAttribute(hConsole, 12);} 
+				else if (map [i][j] == 0) continue; else box = 32;
 			
 			printf ("%c", box);
 			
@@ -88,20 +119,27 @@ int food (int map[yyyyyyyy][xxxxxxxx]) {
 
 int main () {int x=1;
 	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+		char fileAddress [1000];
 	
+	printf ("ENTER MAP FILE ADDRESS : ");
+	gets (fileAddress);
+
+	system ("cls");
 	
-	int map [yyyyyyyy][xxxxxxxx] = {
-									0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-							    	0, 2, 5, 5, 5, 5, 5, 0, 5, 0,
-							    	0, 5, 5, 0, 0, 5, 5, 5, 5, 0,
-							    	0, 5, 5, 5, 5, 5 ,5, 0, 5, 0,
-							    	0, 5, 5, 5, 5, 5, 5, 5, 5, 0,
-							    	0, 5, 5, 0, 5, 5, 5, 5, 5, 0,
-							    	0, 5, 0, 5, 0, 0, 5, 5, 0, 0,
-							    	0, 5, 0, 5, 5, 5, 5, 5, 1, 0,
-							    	0, 6, 5, 5, 5, 5, 5, 5, 5, 0,
-							    	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-								};
+	int map [yyyyyyyy][xxxxxxxx]={0};
+	scanmap (map,fileAddress);
+//	int map [yyyyyyyy][xxxxxxxx] = {
+//									8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+//							    	8, 2, 5, 5, 5, 5, 5, 8, 5, 8,
+//							    	8, 5, 5, 8, 8, 5, 5, 5, 5, 8,
+//							    	8, 5, 5, 5, 5, 5 ,5, 8, 5, 8,
+//							    	8, 5, 5, 5, 5, 5, 5, 5, 5, 8,
+//							    	8, 5, 5, 8, 5, 5, 5, 5, 5, 8,
+//							    	8, 5, 8, 5, 8, 8, 5, 5, 8, 8,
+//							    	8, 5, 8, 5, 5, 5, 5, 5, 1, 8,
+//							    	8, 6, 5, 5, 5, 5, 5, 5, 5, 8,
+//							    	8, 8, 8, 8, 8, 8, 8, 8, 8, 8,
+//								};
 	printmap (map);
 	while(x){
 		move(map);
@@ -116,46 +154,4 @@ getch();
 
 
 
-/*
-int main () {
 
-	char fileAddress [MAX];
-	
-	printf ("ENTER SOURCE FILE ADDRESS : ");
-	gets (fileAddress);
-
-	system ("cls");
-}
-void scanTable (coord* pacman, short table[MCOLUMN][MROW], char const* fileAddress){
-	FILE* source = fopen (fileAddress, "r");
-
-	int i = 0;
-	int j = 0;
-
-	if (source == NULL) {
-		SetConsoleTextAttribute(hConsole, 76);
-		printf ("FIle Not Exist\n");
-		SetConsoleTextAttribute(hConsole, 15);
-		exit (1);
-	}
-	while (!feof (source)){
-		char bfr = getc (source);
-
-		if (bfr == '*') table [i][j++] = 1;
-		if (bfr == '#') table [i][j++] = 0;
-		if (bfr == '1') table [i][j++] = -1;
-		if (bfr == '0') {
-			table [i][j++] = 2;
-			pacman->x = j-1;
-			pacman->y = i;
-		}
-
-		else if (bfr == '\n') {
-			i++;
-			if (j > ROW) ROW = j; j = 0;
-		}
-	}
-}
-
-
-*/
